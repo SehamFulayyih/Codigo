bd95cb5b7298e4f813e16a9c855202500be99df0
SehamFulayyih <Seham.Fulaih.has.s@tuwaiq.edu.sa>
2021-12-27 14:07:16 +0300

    Interface and dark,light mood

--- Codigo/Codigo/Profile/ThemeVC.swift
+++ Codigo/Codigo/Profile/ThemeVC.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class ThemeVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
 
-   
-
+    @IBAction func cgangeDarkMood(_ sender: Any) {
+        
+        let windowScene = UIApplication.shared.connectedScenes.first as! UIWindowScene
+        let window = windowScene.delegate as! SceneDelegate
+        window.window?.overrideUserInterfaceStyle = .dark
+        
+        overrideUserInterfaceStyle = .dark
+    }
+    @IBAction func cgangeLightMood(_ sender: Any) {
+        
+        let windowScene = UIApplication.shared.connectedScenes.first as! UIWindowScene
+        let window = windowScene.delegate as! SceneDelegate
+        window.window?.overrideUserInterfaceStyle = .light
+        
+         overrideUserInterfaceStyle = .light
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+        
+          
+       
+    }
 }
+
